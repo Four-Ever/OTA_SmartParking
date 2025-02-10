1edd733783a59e9ac7ab7851cf2910f9501cbff8
DHniyeo <ehdgns915@gmail.com>
2025-02-10 17:35:44 +0900

    LaneDetection : Camera Calibration Complete

--- jetson/ros2_ws/src/lane_detection/src/main.cpp
+++ jetson/ros2_ws/src/lane_detection/src/main.cpp
@@ -1,11 +1,25 @@
-// src/main.cpp
 #include <rclcpp/rclcpp.hpp>
-#include "lane_detection/lane_detection_node.hpp"
+#include "lane_detection/camera_node.hpp"
+#include "lane_detection/vision_node.hpp"
 
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<LaneDetectionNode>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char * argv[])
+{
+   rclcpp::init(argc, argv);
+   
+   // MultiThreadedExecutor 생성
+   rclcpp::executors::MultiThreadedExecutor executor;
+   
+   // 노드 생성
+   auto camera_node = std::make_shared<CameraNode>();
+   auto vision_node = std::make_shared<VisionNode>();
+   
+   // 노드 추가
+   executor.add_node(camera_node);
+   executor.add_node(vision_node);
+   
+   // 실행
+   executor.spin();
+   
+   rclcpp::shutdown();
+   return 0;
 }
