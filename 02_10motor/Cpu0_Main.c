ab5d64e4f9f77d37c8bd3f9bb04e6c9f1de0066a
daekyounglee <swbubl00@gmail.com>
2025-02-18 09:24:09 +0900

    깨지는 주석은 영어로 수정

--- 02_10motor/Cpu0_Main.c
+++ 02_10motor/Cpu0_Main.c
@@ -26,7 +26,7 @@
 #include "Homo_Coordinate.h"
 
 
-//ToF 헤더
+//ToF
 #include "ToF.h"
 
 //#include "Ifx_IntPrioDef.h"
@@ -34,9 +34,9 @@
 
 /*********************************************************************************************************************/
 /*------------------------------------------------------Macros-------------------------------------------------------*/
-//#define motor_Test //�뿏肄붾뜑 嫄곕━ �솗�씤
-//#define putty_Test //putty uart 硫붿꽭吏� �솗�씤
-//#define tuning_Test //�떆裕щ쭅�겕�뿉�꽌 pid 怨꾩닔 議곗젙 諛� �뒪肄뷀봽 �솗�씤
+//#define motor_Test //
+//#define putty_Test //
+//#define tuning_Test //
 /*********************************************************************************************************************/
 
 /*********************************************************************************************************************/
@@ -143,19 +143,15 @@ int core0_main(void)
     Distance[TOF0]; // uart0 ToF Data
     Distance[TOF1]; // uart2 ToF Data
 
-    // motor_dir = 0;    // 0:정방향, 1:역방향
-    // motor_enable = 0;  // 0:제동, 1:해제
-
 #ifdef motor_Test
-    // motor_enable = 1;  // 0:�젣�룞, 1:�빐�젣
-
+    // motor_enable = 1;
     Kp_s = 1.55f;//1.75f;
     Ki_s = 2.65f;//0.198f;
     Kd_s = 0.001f;
 
     vehicle_status.engine_state = engine_on;
 
-    waitTime(300000000); // 3珥�
+    waitTime(300000000); //
     init_move_distance_control(1000.0f, 500.0f); // 1000mm, 1000rpm
 #endif
 
@@ -163,38 +159,28 @@ int core0_main(void)
     {
         AppScheduling();
         stopstatus=Touch();
-        //can 硫붿꽭吏� 諛쏅뒗 怨�
-#if !defined(motor_Test) && !defined(tuning_Test) && !defined(putty_Test) // 二쇳뻾 肄붾뱶
-        // �뿏吏� 硫붿꽭吏�
+        //can msg 수신
+#if !defined(motor_Test) && !defined(tuning_Test) && !defined(putty_Test) // 雅뚯눛六� �굜遺얜굡
+        // 엔진 on/off
         if (db_flag.CGW_Engine_Flag == 1)
         {
             db_flag.CGW_Engine_Flag = 0;
-            //if (db_msg.CGW_Engine.control_engine==1){
-            //    vehicle_status.engine_state = db_msg.CGW_Engine.control_engine;
-            //}
+
             if (U8DriverState == Parking || U8RSPAState==Parking_Complete || U8DriverState == InitDriverState ){
                 vehicle_status.engine_state = db_msg.CGW_Engine.control_engine;
             }
 
         }
-
-        // �뿏吏꾩씠 耳쒖졇 �엳�쓣 �븣,
+        //엔진 ON
         if (vehicle_status.engine_state == ENGINE_ON)
         {
-            // �닔�룞議곗옉 硫붿꽭吏�
+            //driver mode
             if (db_flag.CGW_Move_Flag == 1)
             {
                 db_flag.CGW_Move_Flag = 0;
-
-                //D_trans = db_msg.CGW_Move.control_transmission;
                 U8IsTrButton = db_msg.CGW_Move.control_transmission;
-                //vehicle_status.transmission = db_msg.CGW_Move.control_transmission;
-                
                 D_steering = db_msg.CGW_Move.control_steering_angle;
-                //vehicle_status.steering_angle = db_msg.CGW_Move.control_steering_angle;
-
-                // �닔�룞 議곗옉 紐⑤뱶濡� �쟾�솚
-                //vehicle_status.user_mode = USER_DRIVE_MODE;
+                
                 if (db_msg.CGW_Move.control_accel == 1) // accel
                 {
                     D_RefRPM += 100.0f;
@@ -216,24 +202,22 @@ int core0_main(void)
                 D_Ref_vel = (double)((D_RefRPM * circumference) / (60 * gear_ratio));
             }
 
-            // �옄�룞 二쇱감 �슂泥�
-
+            //RSPA mode
             if (db_flag.CGW_Auto_Parking_Request_Flag==1)
             {
                 db_flag.CGW_Auto_Parking_Request_Flag=0;
 
                 IsRSPAButton = 1;
-
             }
-            //wp �닔�떊�솗�씤
+
+            //waypoint msg
             if (db_flag.CCU_Cordi_data1_Flag == 1 && db_flag.CCU_Cordi_data2_Flag == 1) {
-                // 紐⑤뱺 �뜲�씠�꽣媛� �뱾�뼱�삩 寃쎌슦
+                //紐⑤뱺 wp 硫붿꽭吏�瑜� �닔�떊�뻽�쓣 �븣
                 db_flag.CCU_Cordi_data1_Flag=0;
                 db_flag.CCU_Cordi_data2_Flag=0;
 
-                // 移대찓�씪 �뵿�� 醫뚰몴 ���옣
-                //if (db_msg.CCU_Cordi_data2.trust_value > 0.7){  //�떊猶곕룄 �넂�쓣寃쎌슦留� 媛믪쓣 �궗�슜�븿.
-                    // �씠�쟾 �뜲�씠�꽣 珥덇린�솕
+                //
+                //if (db_msg.CCU_Cordi_data2.trust_value > 0.7){
                     InitCampoints();
                     cam_points[0][0] = db_msg.CCU_Cordi_data1.cordi_data_x1;
                     cam_points[0][1] = db_msg.CCU_Cordi_data1.cordi_data_y1;
@@ -249,22 +233,22 @@ int core0_main(void)
 
                     data_ready_flag = 1;
                 //}
-                if (U8RSPAState == Searching || U8RSPAState == Backward_Assist) {  //李⑥꽑湲곕컲 二쇳뻾 紐⑤뱶�뒗 �몢媛쒕컰�뿉 �뾾�쓬
+                if (U8RSPAState == Searching || U8RSPAState == Backward_Assist) {  //lane detection mode
                     if (data_ready_flag == 1){
                         InitWorldpoints();
 
-                        if(First_Set==1){   //留� 泥섏쓬�뿉 wp 諛쏆쓣�븣
+                        if(First_Set==1){    //
                             initStanley();
-                            transform_points(H, cam_points, world_points); //醫뚰몴怨� 蹂��솚
+                            transform_points(H, cam_points, world_points);
                             if (transform_finished==1){
-                                updateWaypoints(world_points);  //stanely �쓽 臾숉몴 waypoint 蹂�寃�
+                                updateWaypoints(world_points);
                             }
                             First_Set=0;
                             if (U8RSPAState==Searching){
                                 lanecheck_request=1;
                             }
                         }
-                        else if (IsWPTrackingFinish==1){ // 洹� �씠�썑�뿉�뒗 �씠�쟾 wp 紐⑤몢 異붿쥌�뻽�쓣�븣 �깉濡� 媛깆떊�븿.
+                        else if (IsWPTrackingFinish==1){    //
                             initStanley();
                             transform_points(H, cam_points, world_points);
                             if (transform_finished==1){
@@ -277,7 +261,7 @@ int core0_main(void)
             }
 
 
-            //직각검출
+            //lane detecion_Coner
             if (db_flag.CCU_RightAngle_detect_Flag == 1)
             {
                 db_flag.CCU_RightAngle_detect_Flag = 0;
@@ -285,31 +269,28 @@ int core0_main(void)
             }
         }
 
-        //�뿏吏꾩씠 爰쇱졇 �엳�쓣 �븣,
+        //engine off
         else if (vehicle_status.engine_state == ENGINE_OFF)
         {
             if (db_flag.CGW_Off_Request_Flag==1)
             {
                 db_flag.CGW_Off_Request_Flag=0;
 
-                // 李⑤웾 李얘린 �슂泥�
+                //
                 if (db_msg.CGW_Off_Request.alert_request==1)
                 {
-                    //二쇱감�븳 李⑤웾�씠 �엳�쑝硫� LED �샊�� 遺��� �굪�슜�굪�슜
+                    //find my car_LED/Sound
                     if (U8PrkFinished==1)
                     {
 
                     }
                 }
 
-                // 異쒖감 �슂泥�
+                //출차
                 if (db_msg.CGW_Off_Request.auto_exit_request==1)
                 {
                     vehicle_status.engine_state = ENGINE_ON;
-                    // �떆�뒪�뀥 議곗옉 紐⑤뱶濡� �쟾�솚
-                    //vehicle_status.user_mode = SYSTEM_DRIVE_MODE;
 
-                    //二쇱감�븳 李⑤웾�씠 �엳�쓣 �븣 異쒖감 �슂泥�// �뵳 �븳踰덈쭔 二쇱감-異쒖감
                     if (U8PrkFinished==1)
                     {
                         ExitCAR_request=1;
@@ -335,14 +316,14 @@ void make_can_message(void)
     update_message_engine_status(&db_msg.VCU_Vehicle_Engine_Status, &vehicle_status);
     output_message(&db_msg.VCU_Vehicle_Engine_Status, VCU_Vehicle_Engine_Status_ID);
 
-    if (CameraSwitchRequest != 0) // 1 : �쟾諛�, 2 : �썑諛�
+    if (CameraSwitchRequest != 0) // 1 : front camera, 2 : rear carmera
     {
         db_msg.VCU_Camera.camera_num = CameraSwitchRequest;
         output_message(&db_msg.VCU_Camera, VCU_Camera_ID);
         CameraSwitchRequest = 0;
     }
     if (lanecheck_request != 0) {
-        //�젽�뒯�뿉�꽌 二쇱감�젙吏��꽑�솗�씤 �슂泥�
+        //
     }
 }
 
@@ -404,13 +385,13 @@ void AppTask10ms(void)
 
 void AppTask50ms(void){
     stTestCnt.u32nuCnt50ms++;
-    decision_stateflow_step();  //二쇳뻾紐⑤뱶�뿉 �뵲瑜� 醫낇슒 input 寃곗젙
+    decision_stateflow_step();
 }
 
 void AppTask100ms(void)
 {
     stTestCnt.u32nuCnt100ms++;
-    update_VCU_inputs();  //紐⑦꽣�뿉 �젣�뼱input �쓣 �꽔�뼱以�
+
     now_status = imuRead();
     stanelytheta = now_euler.yaw;
     if (stopstatus == 1)
@@ -422,13 +403,14 @@ void AppTask100ms(void)
         now_euler.yaw = 0;
     }
     now_euler = MadgwickAHRSupdateIMU(now_status);
-    print_encimu(&now_status, &now_euler);
+    //print_encimu(&now_status, &now_euler);
 
-#if (!defined(motor_Test) && !defined(tuning_Test) && !defined(putty_Test)) // 二쇳뻾 肄붾뱶
+    update_VCU_inputs();
+
+#if (!defined(motor_Test) && !defined(tuning_Test) && !defined(putty_Test)) // 雅뚯눛六� �굜遺얜굡
     if (vehicle_status.engine_state == ENGINE_ON)
     {
-
-        //�떆�룞�씠 耳쒖졇�엳�쓣 �븣, can message 異쒕젰
+        //send message
         make_can_message();
     }
 #endif
