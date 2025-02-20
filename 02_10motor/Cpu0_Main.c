2c6ee52ce0396383aecc6a3abd5114e3e33c8242
daekyounglee <swbubl00@gmail.com>
2025-02-20 21:35:11 +0900

    주차 빈공간 인지 코드 추가

--- 02_10motor/Cpu0_Main.c
+++ 02_10motor/Cpu0_Main.c
@@ -141,9 +141,12 @@ int core0_main (void)
 
     alarm_request = 1;
     initStanley();
-    waitTime(100000000);
-    initIMU();
 
+    initIMU();
+    waitTime(100000000);
+    //U8Driver=ModeOn;
+
+    IsRSPAButton = 1;
 #ifdef motor_Test
     // motor_enable = 1;
     Kp_s = 1.55f;//1.75f;
@@ -306,7 +309,7 @@ int core0_main (void)
 //                }
 //            }
 //        }
-//#endif
+////#endif
     }
 
     return 0;
@@ -375,6 +378,58 @@ void initIMU_error (void)
     }
 }
 
+void parking_coded(void){
+    if (md_flag==0) {
+             if(move_distance(300)== REACHED_TARGET_DIS){
+                 md_flag=1;
+             }
+     }
+     else if (md_flag==2){
+         if(move_distance(-300) == REACHED_TARGET_DIS){
+             md_flag=3;
+         }
+
+     }
+     else if (md_flag==4){
+         if(move_distance(180) == REACHED_TARGET_DIS){
+             md_flag=5;
+         }
+
+     }
+
+     else if (md_flag==6){
+         if(move_distance(-200) == REACHED_TARGET_DIS){
+             md_flag=7;
+         }
+
+     }
+     else if (md_flag==8){
+         if(move_distance(-250) == REACHED_TARGET_DIS){
+             md_flag=9;
+         }
+
+     }
+}
+void parking_coded_input(void){
+    if(md_flag == 0 || md_flag == 4) {
+        setServoAngle(-35);
+        RPM_CMD1=2300;  //600
+    }
+    else if(md_flag==1 || md_flag==3 || md_flag == 5 || md_flag == 7 ||md_flag == 9){
+        setServoAngle(0);
+        RPM_CMD1=0;
+        md_flag++;
+    }
+    else if(md_flag==2 || md_flag==6){
+        setServoAngle(+47);
+        RPM_CMD1=-1000;
+    }
+    else if (md_flag == 8){
+        setServoAngle(8);
+        RPM_CMD1=-1000;
+    }
+}
+
 void AppTask1ms (void)
 {
     stTestCnt.u32nuCnt1ms++;
@@ -424,39 +479,10 @@ void AppTask10ms (void)
 void AppTask50ms (void)
 {
     stTestCnt.u32nuCnt50ms++;
-//    Obstacle_get_All_Distance();
-//    decision_stateflow_step();
+    Obstacle_get_All_Distance();
+    decision_stateflow_step();
 
-    if (md_flag==0) {
-            if(move_distance(300)== REACHED_TARGET_DIS){
-                md_flag=1;
-            }
-    }
-    else if (md_flag==2){
-        if(move_distance(-300) == REACHED_TARGET_DIS){
-            md_flag=3;
-        }
 
-    }
-    else if (md_flag==4){
-        if(move_distance(180) == REACHED_TARGET_DIS){
-            md_flag=5;
-        }
-
-    }
-
-    else if (md_flag==6){
-        if(move_distance(-200) == REACHED_TARGET_DIS){
-            md_flag=7;
-        }
-
-    }
-    else if (md_flag==8){
-        if(move_distance(-250) == REACHED_TARGET_DIS){
-            md_flag=9;
-        }
-
-    }
 
 
 }
@@ -464,27 +490,9 @@ void AppTask50ms (void)
 void AppTask100ms (void)
 {
     stTestCnt.u32nuCnt100ms++;
+    //parking_coded_input();
 
-    if(md_flag == 0 || md_flag == 4) {
-        setServoAngle(-35);
-        RPM_CMD1=2300;  //600
-    }
-    else if(md_flag==1 || md_flag==3 || md_flag == 5 || md_flag == 7 ||md_flag == 9){
-        setServoAngle(0);
-        RPM_CMD1=0;
-        md_flag++;
-    }
-    else if(md_flag==2 || md_flag==6){
-        setServoAngle(+47);
-        RPM_CMD1=-1000;
-    }
-    else if (md_flag == 8){
-        setServoAngle(8);
-        RPM_CMD1=-1000;
-    }
-
-
-//    //update_VCU_inputs();
+    update_VCU_inputs();
 //    setServoAngle(gitstanleytest());
 //    RPM_CMD1 = stanleytref_vel;
 //    now_status.accel_x = x;
@@ -497,11 +505,11 @@ void AppTask100ms (void)
 //
 //
 //#if (!defined(motor_Test) && !defined(tuning_Test) && !defined(putty_Test)) //
-//    if (vehicle_status.engine_state == ENGINE_ON)
-//    {
-//        //send message
-//        make_can_message();
-//    }
+    if (vehicle_status.engine_state == ENGINE_ON)
+    {
+        //send message
+        make_can_message();
+    }
 //#endif
 }
 
